15a74ea58d8a5e9ea7b759be6f363b432f83d3ee
subharaj01 <37809579+subharaj01@users.noreply.github.com>
2025-07-16 09:34:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,5 +57,4 @@ if time_to_insert:
 
 
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
